567677bc191ee76e7a5442d394bea93fe74fff55
Shazil Tovar <shazil@gmail.com>
2017-06-02 16:52:39 -0500

    codigo js agregado

--- js/main.js
+++ js/main.js
@@ -2,30 +2,33 @@ $(document).ready(function () {
     $(".modal").modal();
 });
 
-var agregarContacto=document.getElementById("botonPublicar");
-    agregarContacto.addEventListener("submit",function (e){
+var agregarContacto=document.getElementById("botonPublicar");   agregarContacto.addEventListener("click",agregarNuevoContacto);
+
+
+function agregarNuevoContacto(e){
     e.preventDefault();
-    agregarContacto.addEventListener(click,agregarTwitt);
-});
-//obtener datos
-var nombre = document.getElementById("modal-contacto-nombre").value;
-var apellido= document.getElementById("modal-contacto-apellido").value;
-var telefono=document.getElementById("modal-contacto-telefono").value;
+    var contenedorPublicaciones=document.getElementById("main");
     
-function agregarTwitt(){
-    var areaPublicar=document.getElementById("main");
+    var nombre = document.getElementById("modal-contacto-nombre").value;
+    var apellido= document.getElementById("modal-contacto-apellido").value;
+    var telefono=document.getElementById("modal-contacto-telefono").value;
     
     //crear Twitter nuevo y sus elementos
+    var areaPublicarContacto=document.createElement("div");
     var nuevoContactoNombre=document.createElement("p");
     var nuevoContactoApellido=document.createElement("p");
     var nuevoTelefono=document.createElement("p");
     
-    //escribir los textos
-    nuevoContactoNombre.innerHTML=nombre;
-    nuevoContactoApellido.innerHTML=apellido;
-    nuevoContactoTelefono.innerHTML=telefono;
+    nuevoContactoNombre.textContent=nombre;
+    nuevoContactoApellido.textContent=apellido;
+    nuevoTelefono.textContent=telefono;
     
+    areaPublicarContacto.appendChild(nuevoContactoNombre);
+    areaPublicarContacto.appendChild(nuevoContactoApellido);
+    areaPublicarContacto.appendChild(nuevoTelefono);
     
+    contenedorPublicaciones.appendChild(areaPublicarContacto);
+    contenedorPublicaciones.value = "";
     
     
 }
